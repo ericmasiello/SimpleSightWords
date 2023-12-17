650eb4beacaea2b9797c6cd90761694d994646c5
Eric Masiello <emasiello@vistaprint.com>
2023-12-17 06:21:35 -0500

    Initial Commit

---
+++ SimpleSightWords/SimpleSightWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSightWordsApp.swift
+//  SimpleSightWords
+//
+//  Created by Eric Masiello on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSightWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
